cafdc14de7e0377c42d413e69f14897efeec7194
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-11-16 10:36:01 +0000

    20201116

--- variables.tf
+++ variables.tf
@@ -103,7 +103,7 @@ variable "ansible" {
     downloadGoogleDriveObjectTag = "v1.00"
     jsonFileOpenStack = "~/fromTfOpenStack.json"
     osAviControllerUrl = "https://github.com/tacobayle/osAviController"
-    osAviControllerTag = "v1.32"
+    osAviControllerTag = "v1.33"
     osInventoryUrl = "https://raw.githubusercontent.com/openstack/ansible-collections-openstack/master/scripts/inventory/openstack_inventory.py"
     yamlFile = "~/fromTf.yml"
   }
